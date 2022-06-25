a263337f2ecd9ba5e663472dc51a95632f923b4a
Timbogen <niederer.tim@gmail.com>
2022-06-25 11:40:54 +0200

    feat(client): Add responsive behaviour for the about page

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from "@angular/core";
+import { Component, ElementRef, OnDestroy } from "@angular/core";
 import createScrollSnap from "scroll-snap";
 
 @Component({
@@ -6,15 +6,43 @@ import createScrollSnap from "scroll-snap";
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+    /**
+     * The reference to the scroll snap
+     */
+    private scrollSnap?: { unbind: () => void };
+    /**
+     * The media query to enable/disable scroll snapping
+     */
+    private readonly mediaQuery: MediaQueryList = window.matchMedia("(max-width: 760px)");
+
     /**
      * Constructor
      */
     constructor(private readonly elementRef: ElementRef) {
-        createScrollSnap(elementRef.nativeElement, {
-            snapDestinationY: "100%",
-            threshold: 0.01,
-            timeout: 0.01,
-        });
+        this.updateScrollSnapping();
+        this.mediaQuery.addEventListener("change", this.updateScrollSnapping);
     }
+
+    /**
+     * Remove the media query
+     */
+    public ngOnDestroy(): void {
+        this.mediaQuery.removeEventListener("change", this.updateScrollSnapping);
+    }
+
+    /**
+     * Enable/Disable scroll snapping
+     */
+    private updateScrollSnapping = (): void => {
+        if (this.mediaQuery.matches) {
+            if (this.scrollSnap) this.scrollSnap.unbind();
+        } else {
+            this.scrollSnap = createScrollSnap(this.elementRef.nativeElement, {
+                snapDestinationY: "100%",
+                threshold: 0.01,
+                timeout: 0.01,
+            });
+        }
+    };
 }
