96c9d4b0341a907c7f196cfe8aa9f9b990e919f7
Tim Niederer <niederer.tim@gmail.com>
2022-07-10 20:49:27 +0200

    feat(client): Improve the scroll snapping for the home page

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,49 +1,29 @@
-import { Component, ElementRef, OnDestroy } from "@angular/core";
-import createScrollSnap from "scroll-snap";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-home",
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnDestroy {
+export class HomeComponent {
     /**
-     * The reference to the scroll snap
+     * The shown sections
      */
-    private scrollSnap?: { unbind: () => void };
+    private sections = ["about", "youtube", "clothes", "gallery", "others"];
     /**
-     * The media query to enable/disable scroll snapping
+     * The timeout reference for the scroll snapping
      */
-    private readonly mediaQuery: MediaQueryList = window.matchMedia("(max-width: 760px)");
+    private scrollTimeout = NaN;
 
     /**
      * Constructor
      */
-    constructor(private readonly elementRef: ElementRef) {
-        this.updateScrollSnapping();
-        this.mediaQuery.addEventListener("change", this.updateScrollSnapping);
+    constructor() {
+        document.addEventListener("scroll", () => {
+            clearTimeout(this.scrollTimeout);
+            this.scrollTimeout = setTimeout(this.scrollToSection, 1000);
+        });
     }
-    /**
-     * Remove the media query
-     */
-    public ngOnDestroy(): void {
-        this.mediaQuery.removeEventListener("change", this.updateScrollSnapping);
-    }
-
-    /**
-     * Enable/Disable scroll snapping
-     */
-    private updateScrollSnapping = (): void => {
-        if (this.mediaQuery.matches) {
-            if (this.scrollSnap) this.scrollSnap.unbind();
-        } else {
-            this.scrollSnap = createScrollSnap(this.elementRef.nativeElement, {
-                snapDestinationY: "100%",
-                threshold: 0.01,
-                timeout: 0.01,
-            });
-        }
-    };
 
     /**
      * Scroll into a section
@@ -52,4 +32,15 @@ export class HomeComponent implements OnDestroy {
         const element = document.getElementById(section);
         if (element) element.scrollIntoView({ behavior: "smooth", block: "start" });
     }
+
+    /**
+     * Try to scroll to the nearest section
+     */
+    private scrollToSection = () => {
+        for (const sectionId of this.sections) {
+            const section = document.getElementById(sectionId) as HTMLElement;
+            const relativePosition = section.offsetTop - document.documentElement.scrollTop;
+            if (Math.abs(relativePosition) < 200) this.scrollTo(sectionId);
+        }
+    };
 }
